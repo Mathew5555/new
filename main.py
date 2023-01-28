0f9e9c7516dab7e2606248c2596934ca48e126a0
HappyMatDuckling <matveyd05@yandex.ru>
2023-01-28 22:52:38 +0300

    - Капучино

--- main.py
+++ main.py
@@ -3,6 +3,88 @@ from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem, QHeaderView, QFileDia
 from PyQt5 import QtWidgets
 import sqlite3
 from PyQt5 import uic
+from PyQt5 import QtGui
+
+
+class Add_Coffee(QDialog):
+    def __init__(self, obj):
+        self.obj = obj
+        super().__init__()
+        uic.loadUi("addEditCoffeeForm.ui", self)
+        self.setFixedSize(611, 373)
+        self.con = sqlite3.connect("coffee.db")
+        self.add.clicked.connect(self.new_func)
+        self.cur = self.con.cursor()
+        self.comboBox.addItems([el[1] for el in self.cur.execute('select * from roast').fetchall()])
+        self.comboBox_2.addItems(["в зёрнах", "молотый"])
+
+    def new_func(self):
+        try:
+            title = self.lineEdit_3.text()
+            roast = self.comboBox.currentText()
+            structure = self.comboBox_2.currentText()
+            price = int(self.spinBox.text())
+            vol = int(self.spinBox_2.text())
+            if not title:
+                raise Exception
+            id = list(self.cur.execute(f"SELECT id FROM coffe_info").fetchall())[-1][0] + 1
+            roast_id = int(list(self.cur.execute(f"SELECT id from roast where '{roast}' = roasted").fetchall())[0][0])
+            msg = QMessageBox.information(self, 'Успешно!', f'Добавлен элемент', QMessageBox.Ok)
+            request = ", ".join([])
+            if structure == "молотый":
+                structure = 1
+            else:
+                structure = 0
+            self.cur.execute(f"""INSERT INTO coffe_info("id", "title", "roasted", "structure", "price", "volume") 
+            VALUES({id}, '{title}', {roast_id}, {structure}, {price}, {vol})""")
+            self.close()
+            self.con.commit()
+            self.cur.close()
+            self.obj.table_run()
+        except Exception as e:
+            print("Ошибка")
+            self.close()
+
+
+class Update_coffee(QDialog):
+    def __init__(self, obj, id):
+        self.obj = obj
+        self.title = id
+        super().__init__()
+        uic.loadUi("addEditCoffeeForm.ui", self)
+        self.setFixedSize(611, 373)
+        self.con = sqlite3.connect("coffee.db")
+        self.add.clicked.connect(self.new_func)
+        self.cur = self.con.cursor()
+        self.comboBox.addItems([el[1] for el in self.cur.execute('select * from roast').fetchall()])
+        self.comboBox_2.addItems(["в зёрнах", "молотый"])
+
+    def new_func(self):
+        try:
+            title = self.lineEdit_3.text()
+            roast = self.comboBox.currentText()
+            structure = self.comboBox_2.currentText()
+            price = int(self.spinBox.text())
+            vol = int(self.spinBox_2.text())
+            if not title:
+                raise Exception
+            roast_id = int(list(self.cur.execute(f"SELECT id from roast where '{roast}' = roasted").fetchall())[0][0])
+            msg = QMessageBox.information(self, 'Успешно!', f'Обновлен элемент', QMessageBox.Ok)
+            request = ", ".join([])
+            if structure == "молотый":
+                structure = 1
+            else:
+                structure = 0
+            self.cur.execute(f"""UPDATE coffe_info SET title = '{title}', roasted = {roast_id}, structure = {structure},
+             price = {price}, volume = {vol} WHERE title = '{self.title}'""")
+            self.close()
+            self.con.commit()
+            self.cur.close()
+            self.obj.table_run()
+        except Exception as e:
+            print("Ошибка")
+            print(e)
+            self.close()
 
 
 class Main_Table_Window(QMainWindow):
@@ -12,8 +94,8 @@ class Main_Table_Window(QMainWindow):
         self.cur = con.cursor()
         uic.loadUi('main.ui', self)
         self.table.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
-        # self.table.cellDoubleClicked.connect(self.edit_olympiad)
-        # self.add.clicked.connect(self.add)
+        self.table.cellDoubleClicked.connect(self.edit_co)
+        self.add.clicked.connect(self.add_co)
         self.table_run()
 
     def table_run(self):
@@ -36,9 +118,13 @@ class Main_Table_Window(QMainWindow):
         column_head = self.table.horizontalHeader()
         column_head.setSectionResizeMode(0, QHeaderView.Stretch)
 
-    # def add_ol(self):
-    #     self.add_window = Add_olympiad(ww.USER_ID)
-    #     self.add_window.show()
+    def add_co(self):
+        self.add_window = Add_Coffee(self)
+        self.add_window.show()
+
+    def edit_co(self, row, col):
+        self.update_co = Update_coffee(self, self.table.item(row, 0).text())
+        self.update_co.show()
 
 
 if __name__ == '__main__':
